ae0f252e77b8a10ac30a10592dec49fd27e7ed28
savannahloiselle <101598825+savannahloiselle@users.noreply.github.com>
2023-03-22 11:35:59 -0400

    Update hello.java

--- hello.java
+++ hello.java
@@ -5,9 +5,11 @@ public class hello {
     public static void main(String [] args) {
 
        // initialize variables
-       String firstName = "Alex";
+       String firstName = "Savannah";
 	
        // Using the value of the variable above, output a message in the format, "Hello, I am Alex".
+	   
+	   System.out.println("Hello, I am " + firstName);
 
     }
 }
